067023aea135f88addd393f8df00b5e2a8f2d648
Arcadeena <mattmcc97@gmail.com>
2021-07-17 14:07:41 +0930

    Update ControlPanelEmbed.ts

--- src/fun/markov/message/embeds/ControlPanelEmbed.ts
+++ src/fun/markov/message/embeds/ControlPanelEmbed.ts
@@ -1,7 +1,7 @@
 import { MarkovChannelSchema, MarkovStringTotals } from '../../MarkovDatabase';
 import { HandlerContext } from 'discord.js-commands';
+import { GuildChannel, Util } from 'discord.js';
 import { MarkovEmbed } from './MarkovEmbed';
-import { GuildChannel } from 'discord.js';
 
 export class ControlPanelEmbed extends MarkovEmbed {
 
@@ -18,8 +18,8 @@ export class ControlPanelEmbed extends MarkovEmbed {
             `Total Messages: **${totals.total}**`,
             `User Messages: **${totals.users}**`,
             'Post frequency:',
-            `- One in \`${channelData.messages}\` messages (\`${Math.round(100 / channelData.messages)}%\` chance per message)`,
-            `- Once every \`${channelData.minutes}\` minutes (\`${Math.round(100 / channelData.minutes)}%\` chance per minute)`
+            `- One in \`${channelData.messages}\` messages (\`${Util.formatDecimal(100 / channelData.messages, 2)}%\` chance per message)`,
+            `- Once every \`${channelData.minutes}\` minutes (\`${Util.formatDecimal(100 / channelData.minutes, 2)}%\` chance per minute)`
         ].join('\n'), false)
     }
 }
