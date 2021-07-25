e7c91131fe53e6de635d44e0ca9bbb194cc9ce44
Arcadeena <mattmcc97@gmail.com>
2021-07-25 20:16:58 +0930

    nsfw

--- src/commands/GuildHandler.ts
+++ src/commands/GuildHandler.ts
@@ -1,6 +1,7 @@
 import { Guild, ApplicationCommand, GuildChannel, GuildMember, InteractionReplyOptions } from 'discord.js';
-import { HandlerContext, HandlerCustomData } from 'discord.js-commands';
+import { HandlerAttachment, HandlerContext, HandlerCustomData } from 'discord.js-commands';
 import { GlobalHandler, GlobalHandlerOptions } from '../..';
+import * as fs from 'fs';
 
 export enum GuildHandlerGroup {
     BOORU = 'Booru',
@@ -24,7 +25,7 @@ export class GuildHandler<T extends HandlerCustomData> extends GlobalHandler<T> 
         return Boolean(await this.fetchCommand(<Guild>context.guild))
     }
 
-    public override async isAuthorised(context: HandlerContext, _customData?: HandlerCustomData): Promise<string[]> {
+    public override async isAuthorised(context: HandlerContext, _customData?: T): Promise<string[]> {
         const { member, channel } = <{ member: GuildMember, channel: GuildChannel }>context;
         const permissions = member.permissionsIn(channel);
         return permissions.missing(this.permissions);
@@ -39,14 +40,28 @@ export class GuildHandler<T extends HandlerCustomData> extends GlobalHandler<T> 
         return found || null;
     }
 
-    public async enable(context: HandlerContext, _customData?: HandlerCustomData): Promise<ApplicationCommand | null> {
+    public async enable(context: HandlerContext, _customData?: T): Promise<ApplicationCommand | null> {
         const found = await this.fetchCommand(context.guild!);
         return found || context.guild!.commands.create(this.commandData);
     }
 
-    public async disable(context: HandlerContext, _customData?: HandlerCustomData): Promise<ApplicationCommand | null> {
+    public async disable(context: HandlerContext, _customData?: T): Promise<ApplicationCommand | null> {
         const found = await this.fetchCommand(context.guild!);
         return found ? found.delete() : null;
+    }
+
+    public override getHandlerNSFWResponse(context: HandlerContext, customData?: T): InteractionReplyOptions {
+        // const type = this.getContextName(context);
+
+        const buffer = fs.readFileSync(`${__dirname}/../../res/thumbnails/nsfw.png`);
+        const attachment = new HandlerAttachment(buffer, 'nsfw.png');
+        const embed = this.getEmbedTemplate(context, customData)
+            .setThumbnail(attachment.getEmbedUrl())
+            .setDescription([
+                `Crikey! I'm not so sure \`${this.id}\` is safe enough to use here.`,
+                'Lets move this to a nsfw channel where we can go crazy!'
+            ].join('\n'));
+        return { embeds: [embed], files: [attachment] }
     }
 
     public getNotFoundResponse(context: HandlerContext, query: string): InteractionReplyOptions {
