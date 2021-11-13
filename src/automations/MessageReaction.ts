3571c3526c3548bf3e9033d226c63ed98de1c2b9
maj0rpain <fatslobwithafatknob@gmail.com>
2021-11-12 20:00:41 -0600

    catch

--- src/automations/MessageReaction.ts
+++ src/automations/MessageReaction.ts
@@ -4,7 +4,7 @@ export function MessageReaction(client: Client) {
 	client.on('messageCreate', (message: Message) => {
 		if (message.content.includes('[LINK]')) {
 			const reactEmoji = message.client.emojis.cache.get('639961055157813309')!;
-			message.react(reactEmoji);
+			message.react(reactEmoji).catch(()=>{});
 		}
 	})
 }
