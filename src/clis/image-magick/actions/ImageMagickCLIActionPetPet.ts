1a7ef6d55c4d4456af999f557e0ee8352c8ee7c1
Arcadeena <mattmcc97@gmail.com>
2021-11-29 20:58:32 +1030

    clean

--- src/clis/image-magick/actions/ImageMagickCLIActionPetPet.ts
+++ src/clis/image-magick/actions/ImageMagickCLIActionPetPet.ts
@@ -23,31 +23,32 @@ export class ImageMagickCLIActionPetPet extends ImageMagickCLIAction {
             '-background', 'black',
             '-gravity', 'south',
 
-            `${path.resolve()}/res/magick/pet_pet_0.png`,                              // 0
-            `${path.resolve()}/res/magick/pet_pet_1.png`,                              // 1
-            `${path.resolve()}/res/magick/pet_pet_2.png`,                              // 2
-            `${path.resolve()}/res/magick/pet_pet_3.png`,                              // 3
-            `${path.resolve()}/res/magick/pet_pet_4.png`,                              // 4
-            `${path.resolve()}/res/magick/pet_pet_5.png`,                              // 5
-            `${path.resolve()}/res/magick/pet_pet_6.png`,                              // 6
-            metadata.url,                                                              // 7
-            '(', '-clone', '7', '-resize', '500x500^', ')',                            // 8 - Force input image to be min 500x500 on both axis
+            `${path.resolve()}/res/magick/pet_pet_0.png`,                                 // 0
+            `${path.resolve()}/res/magick/pet_pet_1.png`,                                 // 1
+            `${path.resolve()}/res/magick/pet_pet_2.png`,                                 // 2
+            `${path.resolve()}/res/magick/pet_pet_3.png`,                                 // 3
+            `${path.resolve()}/res/magick/pet_pet_4.png`,                                 // 4
+            `${path.resolve()}/res/magick/pet_pet_5.png`,                                 // 5
+            `${path.resolve()}/res/magick/pet_pet_6.png`,                                 // 6
+            metadata.url,                                                                 // 7
+            '(', '-clone', '7', '-resize', '500x500^', ')',                               // 8 - Force input image to be min 500x500 on both axis
 
-            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117%', ')',    // 9
-            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117%', ')',    // 10
-            '(', '-clone', '8', '-resize', '100%x75%', '-extent', '100%x133%', ')',    // 11
-            '(', '-clone', '8', '-resize', '100%x75%', '-extent', '100%x133%', ')',    // 12
-            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117%', ')',    // 13
-            '(', '-clone', '8', '-resize', '100%x95%', '-extent', '100%x105%', ')',    // 14
+            // -extent is equal 1 / -resize percentage
+            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117.65%', ')',    // 9
+            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117.65%', ')',    // 10
+            '(', '-clone', '8', '-resize', '100%x75%', '-extent', '100%x133.33%', ')',    // 11
+            '(', '-clone', '8', '-resize', '100%x75%', '-extent', '100%x133.33%', ')',    // 12
+            '(', '-clone', '8', '-resize', '100%x85%', '-extent', '100%x117.65%', ')',    // 13
+            '(', '-clone', '8', '-resize', '100%x95%', '-extent', '100%x105.26%', ')',    // 14
 
             // Clone the hands and overlay them on the new image
-            '(', '-clone', '8', '-clone', '0', '-flatten', ')',                        // 14
-            '(', '-clone', '9', '-clone', '1', '-flatten', ')',                        // 15
-            '(', '-clone', '10', '-clone', '2', '-flatten', ')',                       // 16
-            '(', '-clone', '11', '-clone', '3', '-flatten', ')',                       // 17
-            '(', '-clone', '12', '-clone', '4', '-flatten', ')',                       // 18
-            '(', '-clone', '13', '-clone', '5', '-flatten', ')',                       // 19
-            '(', '-clone', '14', '-clone', '6', '-flatten', ')',                       // 20
+            '(', '-clone', '8', '-clone', '0', '-flatten', ')',                           // 14
+            '(', '-clone', '9', '-clone', '1', '-flatten', ')',                           // 15
+            '(', '-clone', '10', '-clone', '2', '-flatten', ')',                          // 16
+            '(', '-clone', '11', '-clone', '3', '-flatten', ')',                          // 17
+            '(', '-clone', '12', '-clone', '4', '-flatten', ')',                          // 18
+            '(', '-clone', '13', '-clone', '5', '-flatten', ')',                          // 19
+            '(', '-clone', '14', '-clone', '6', '-flatten', ')',                          // 20
 
             '-delete', '0-14',
 
