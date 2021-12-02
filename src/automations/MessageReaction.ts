97467013bbeefb469d0f6ff7bd15343d08694b60
CringehubThrowaway <dumbgithubthrowaway@gmail.com>
2021-12-02 09:11:05 -0800

    Adding random emote instead of always borpaU

--- src/automations/MessageReaction.ts
+++ src/automations/MessageReaction.ts
@@ -2,6 +2,15 @@ import { Client, DiscordAPIError, Message } from 'discord.js';
 
 export class MessageReaction {
 
+    private static CumEmotes: Array<string> = [
+        '916013266537447465', // CUMDETECTED
+		'915996947695935528', // BorpaU
+		'912127935526346752', // floorpaU
+		'906857271387095041', // cumface
+		'824077626708328479', // borpa
+		'858846864185753642', // borpaspin
+    ];
+
     public static setup(client: Client): void {
         client.on('messageCreate', (message: Message) => {
             // Reacts with Libido when [LINK] is detected
@@ -15,9 +24,10 @@ export class MessageReaction {
                 });
             }
 
-			// Reacts with CUMDETECTED when any combination of "cum" is detected
+			// Reacts with random borpa emote when any combination of "cum" is detected
 			if (message.content.match(/\b[c]+[u]+[m]+\b/i) != null) {
-				const reactEmoji = message.client.emojis.cache.get('880619557373485106')!;
+				const CumEmote = MessageReaction.CumEmotes[MessageReaction.CumEmotes.length * Math.random() << 0]!;
+				const reactEmoji = message.client.emojis.cache.get(CumEmote)!;
                 message.react(reactEmoji).catch(error => {
                     // User has bot blocked and permission to react is denied...
                     if (!(error instanceof DiscordAPIError) || error.httpStatus !== 403) {
