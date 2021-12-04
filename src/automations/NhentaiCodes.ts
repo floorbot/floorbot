4bf4dbdce07cd623207bfc190c6d07cfac38e67d
Arcadeena <mattmcc97@gmail.com>
2021-12-04 12:50:42 +1030

    safety first

--- src/automations/NhentaiCodes.ts
+++ src/automations/NhentaiCodes.ts
@@ -5,22 +5,26 @@ export class NhentaiCodes {
 
 
 
-public static setup(client: Client): void {
+  public static setup(client: Client): void {
 
-  client.on('messageCreate', (check) => {
-    const text = check.content;
-    const hentai = text.match(/(^| )\d{6}($| )/);
-    if (check.author.bot) return
-    if (hentai) {
-        fetch('https://nhentai.net/g/'+hentai[0]+'/')
-          .then(res => {
-            if(res.ok) {
-              check.channel.send('https://nhentai.net/g/'+hentai[0]+'/')
-            } else {
-              check.react('840176060364226590')
-            }
-          });
+    client.on('messageCreate', (check) => {
+      try {
+        const text = check.content;
+        const hentai = text.match(/(^| )\d{6}($| )/);
+        if (check.author.bot) return
+        if (hentai) {
+          fetch('https://nhentai.net/g/' + hentai[0] + '/')
+            .then(res => {
+              if (res.ok) {
+                check.channel.send('https://nhentai.net/g/' + hentai[0] + '/')
+              } else {
+                check.react('840176060364226590')
+              }
+            });
+        }
+      } catch(error){
+        console.log('[nhentai] this needs fixing... eventually...')
       }
-  });
- }
+    });
+  }
 }
