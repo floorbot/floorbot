25efccc82c3e60e2a838abb7530114c27fc643c9
maj0rpain <fatslobwithafatknob@gmail.com>
2021-12-18 10:30:50 -0600

    yikes who made this ugh it sucks

--- src/commands/fun/dispute/DisputeHandler.ts
+++ src/commands/fun/dispute/DisputeHandler.ts
@@ -9,6 +9,10 @@ import { DisputeCommandData } from './DisputeCommandData.js';
 import { DisputeEmbed } from './components/DisputeEmbed.js';
 import { HandlerDB } from '../../../lib/discord/helpers/HandlerDatabase.js';
 
+interface voteStrings {
+    readonly yes_array: string[],
+    readonly no_array: string[];
+}
 export class DisputeHandler extends ContextMenuHandler {
 
     private readonly database: DisputeDatabase;
@@ -23,19 +27,26 @@ export class DisputeHandler extends ContextMenuHandler {
         const origMessage = contextMenu.options.getMessage('message', true) as Message;
         if (!origMessage.content.length) return contextMenu.reply(HandlerReplies.createMessageContentReply(contextMenu, 'dispute'));
         if (origMessage.author == contextMenu.user) return contextMenu.reply(DisputeEmbed.getSelfUsedEmbed(contextMenu));
-        const disputeExists = await this.database.disputeExists(origMessage);
-        const disputeResultsErr = await this.database.fetchResults(origMessage);
-        if (disputeExists) return contextMenu.reply(DisputeEmbed.getAlreadyDisputedEmbed(contextMenu, disputeResultsErr!));
-        await this.database.setDisputeVote(contextMenu, contextMenu, origMessage, true);
+        let disputeResults = await this.database.fetchResults(origMessage);
+        if (disputeResults?.total_votes || 0 > 0) return contextMenu.reply(DisputeEmbed.getAlreadyDisputedEmbed(contextMenu, disputeResults!));
+        await this.database.setDisputeVote(contextMenu, contextMenu.user, origMessage, true);
         await contextMenu.deferReply();
-        const disputeResults = await this.database.fetchResults(origMessage);
-        const yes_string = await this.database.getVoters(origMessage, 'yes');
-        const no_string = await this.database.getVoters(origMessage, 'no');
-        const replyOptions = this.createCurrentResponse(contextMenu, origMessage, disputeResults!, yes_string!, no_string!);
+        disputeResults = await this.database.fetchResults(origMessage);
+        const votes = await this.createVoteStrings(origMessage);
+        const replyOptions = this.createCurrentResponse(contextMenu, origMessage, disputeResults!, votes);
         const message = await contextMenu.followUp(replyOptions) as Message;
         const collector = message.createMessageComponentCollector({ idle: this.endDelay });
         collector.on('collect', this.createCollectorFunction(contextMenu, origMessage));
         collector.on('end', (collection: Collection<string, MessageComponentInteraction>) => { this.onCollectEnd(contextMenu, message, origMessage, collection); });
+    }
+
+    private async createVoteStrings(message: Message): Promise<voteStrings> {
+        const votes = await this.database.getVoters(message);
+        const yes_array: string[] = [];
+        const no_array: string[] = [];
+        if (!votes) return { yes_array, no_array };
+        votes.forEach(function (obj) { obj.vote_choice ? yes_array.push(`<@${obj.vote_user_id}>`) : no_array.push(`<@${obj.vote_user_id}>`); });
+        return { yes_array, no_array };
     }
 
     private createCollectorFunction(contextMenu: ContextMenuInteraction, message: Message): (component: MessageComponentInteraction) => void {
@@ -45,25 +56,23 @@ export class DisputeHandler extends ContextMenuHandler {
                     switch (component.customId) {
                         case DisputeButtonID.YES: {
                             const currVote = await this.database.getDisputeVote(component, message);
-                            if (!currVote) await this.database.setDisputeVote(contextMenu, component, message, true);
+                            if (!currVote) await this.database.setDisputeVote(contextMenu, component.user, message, true);
                             await component.deferUpdate();
                             const disputeResults = await this.database.fetchResults(message);
-                            const yes_string = await this.database.getVoters(message, 'yes');
-                            const no_string = await this.database.getVoters(message, 'no');
+                            const votes = await this.createVoteStrings(message);
                             const newTargetTimestamp = component.createdTimestamp + this.endDelay;
-                            const replyOptions = this.createCurrentResponse(contextMenu, message, disputeResults!, yes_string!, no_string!, newTargetTimestamp);
+                            const replyOptions = this.createCurrentResponse(contextMenu, message, disputeResults!, votes, newTargetTimestamp);
                             await component.editReply(replyOptions);
                             break;
                         }
                         case DisputeButtonID.NO: {
                             const currVote = await this.database.getDisputeVote(component, message);
-                            if (!currVote) await this.database.setDisputeVote(contextMenu, component, message, false);
+                            if (!currVote) await this.database.setDisputeVote(contextMenu, component.user, message, false);
                             await component.deferUpdate();
                             const disputeResults = await this.database.fetchResults(message);
-                            const yes_string = await this.database.getVoters(message, 'yes');
-                            const no_string = await this.database.getVoters(message, 'no');
+                            const votes = await this.createVoteStrings(message);
                             const newTargetTimestamp = component.createdTimestamp + this.endDelay;
-                            const replyOptions = this.createCurrentResponse(contextMenu, message, disputeResults!, yes_string!, no_string!, newTargetTimestamp);
+                            const replyOptions = this.createCurrentResponse(contextMenu, message, disputeResults!, votes, newTargetTimestamp);
                             await component.editReply(replyOptions);
                             break;
                         }
@@ -73,8 +82,9 @@ export class DisputeHandler extends ContextMenuHandler {
         };
     }
 
-    private createCurrentResponse(interaction: Interaction, message: Message, results: DisputeResults, yes_string: string, no_string: string, targetTimestamp: number = 0): InteractionReplyOptions {
-        const embed = DisputeEmbed.getCurrentEmbed(interaction, message, results, yes_string, no_string, targetTimestamp);
+    private createCurrentResponse(interaction: Interaction, message: Message, results: DisputeResults, votes: voteStrings, targetTimestamp: number = 0): InteractionReplyOptions {
+        const { yes_array, no_array } = votes;
+        const embed = DisputeEmbed.getCurrentEmbed(interaction, message, results, yes_array.join('\n'), no_array.join('\n'), targetTimestamp);
         const actionRow: MessageActionRow = new MessageActionRow().addComponents([
             DisputeButton.createDisputeButton(DisputeButtonID.YES),
             DisputeButton.createDisputeButton(DisputeButtonID.NO)
@@ -92,12 +102,10 @@ export class DisputeHandler extends ContextMenuHandler {
         } else {
             if (disputeResults!.yes_votes == disputeResults!.no_votes) {
                 const random_boolean = Math.random() < 0.5;
-                await this.database.setDisputeVoteID(contextMenu, contextMenu.client.user!, origMessage, random_boolean);
+                await this.database.setDisputeVote(contextMenu, contextMenu.client.user!, origMessage, random_boolean);
                 disputeResults = await this.database.fetchResults(origMessage);
-
-                const yes_string = await this.database.getVoters(origMessage, 'yes');
-                const no_string = await this.database.getVoters(origMessage, 'no');
-                tieReplyOptions = this.createCurrentResponse(contextMenu, origMessage, disputeResults!, yes_string!, no_string!);
+                const votes = await this.createVoteStrings(message);
+                tieReplyOptions = this.createCurrentResponse(contextMenu, origMessage, disputeResults!, votes);
                 await contextMenu.editReply(tieReplyOptions);
             }
             const embed = DisputeEmbed.getFinalEmbed(contextMenu, origMessage, disputeResults!);
